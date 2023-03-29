6fcdc474afcab0a7ae49d9e893f4fe6cd46eaf95
neverFeltAlive <20This.Is.My.Mail00@proronmail.com>
2023-03-29 21:45:34 +0300

    changed project configuration

--- bin/cli.js
+++ bin/cli.js
@@ -3,39 +3,49 @@
 const { execSync } = require('child_process');
 const { program } = require('commander');
 const prompts = require('prompts');
-const { unlink, writeFile } = require('fs');
-const { resolve } = require('path');
+const { unlinkSync, writeFile } = require('fs');
+const { join, resolve, basename } = require('path');
 
 // Configure Commander options
 program
-    .option('--no-index-page, -i', 'project will be configured to autogenerate index page')
+  .option('--no-index-page, -i', 'project will be configured to autogenerate index page');
 
 // Get user options
 program.parse(process.argv);
 const options = program.opts();
 
-// Prompt user for index page options
-if (!options.indexPage){
-  const userPrompt = await indexPagePrompt();
-  options.indexPage = userPrompt.index;
-}
+(async () => {
+  let repoName = program.args[0];
 
-const repoName = program.args[0];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/neverFeltAlive/vite-pug.git ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+  // If project name is not specified
+  if (repoName === undefined) {
+    let userPrompt = await projectNamePrompt();
 
-// Configure pages plugin
-configurePages(options.indexPage);
+    repoName = userPrompt.projectName;
+  }
 
-// Create project
-console.log(`Creating a project: ${repoName}`);
-const checkOut = runCommand(gitCheckoutCommand);
-if (!checkOut) process.exit(-1);
+  // Prompt user for index page options
+  if (!options.indexPage) {
+    const userPrompt = await indexPagePrompt();
+    options.indexPage = userPrompt.index;
+  }
 
-// Install dependencies
-console.log(`Installing dependencies`);
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(-1);
+  const gitCheckoutCommand = `git clone --depth 1 https://github.com/neverFeltAlive/vite-pug.git ${repoName}`;
+  const installDepsCommand = `cd ${repoName} && npm install`;
+
+  // Create project
+  console.log(`Creating a project: ${repoName === '.' ? basename(process.cwd()) : repoName}`);
+  const checkOut = runCommand(gitCheckoutCommand);
+  if (!checkOut) process.exit(-1);
+
+  // Configure pages plugin
+  configurePages(options.indexPage, repoName);
+
+  // Install dependencies
+  console.log(`Installing dependencies`);
+  const installedDeps = runCommand(installDepsCommand);
+  if (!installedDeps) process.exit(-1);
+})();
 
 
 /**
@@ -43,7 +53,7 @@ if (!installedDeps) process.exit(-1);
  * @param command
  * @return {boolean}
  */
-function runCommand (command) {
+function runCommand(command) {
   try {
     execSync(`${command}`, { stdio: 'inherit' });
   } catch (e) {
@@ -57,32 +67,63 @@ function runCommand (command) {
  * Prompts user with index page options
  * @return {Promise<*>}
  */
-async function indexPagePrompt () {
+async function indexPagePrompt() {
   return await prompts({
-    type: 'multiselect',
+    type: 'toggle',
     name: 'index',
     message: 'Do you want to generate index page automatically?',
-    choices: [
-      { title: 'Yes (each time a project is build the index page will be automatically generated)', value: true},
-      { title: 'No (no index page will be generated)', value: false},
-    ]
+    initial: true,
+    active: 'yes',
+    inactive: 'no',
   });
 }
 
-function configurePages (enableIndexPage) {
+/**
+ * Prompts user with index page options
+ * @return {Promise<*>}
+ */
+async function projectNamePrompt() {
+  const cwd = basename(process.cwd());
+  const result = await prompts([
+    {
+      type: 'text',
+      name: 'projectName',
+      message: 'What is the name of your project? (Current directory will be used by default)',
+      initial: cwd,
+    },
+    {
+      type: 'confirm',
+      name: 'isConfirmed',
+      message: (prev) => prev === cwd ? 'To use the current directory make sure that it is empty. Continue?' : `Please confirm the name of the project: ${prev}`,
+      initial: true,
+    },
+  ]);
+
+  !result?.isConfirmed && console.log('Current directory will be used');
+  result.projectName = result.projectName === cwd || !result?.isConfirmed ? '.' : result.projectName;
+  return result;
+}
+
+/**
+ * Generates json config file for pages plugin
+ * @param enableIndexPage
+ * @param dir
+ */
+function configurePages(enableIndexPage, dir) {
   const json = {
-    enableIndexPage
+    enableIndexPage,
+  };
+
+  const cwd = dir === '.' ? resolve(process.cwd()) : resolve(process.cwd(), dir);
+  if (!enableIndexPage) {
+    const indexPagePath = resolve(cwd, 'src', 'pages');
+    unlinkSync(join(indexPagePath, 'index.js'));
+    unlinkSync(join(indexPagePath, 'index.pug'));
+    unlinkSync(join(indexPagePath, 'pages.json'));
   }
 
-  if (!enableIndexPage){
-    const indexPagePath = resolve(__dirname, 'src', 'pages');
-    unlink(indexPagePath + 'index.js', () => {});
-    unlink(indexPagePath + 'index.pug', () => {});
-    unlink(indexPagePath + 'pages.json', () => {});
-  }
-
-  writeFile('pagesconfig.json', JSON.stringify(json), 'utf8', (error) => {
+  writeFile(join(cwd, 'pagesconfig.json'), JSON.stringify(json), 'utf8', (error) => {
     error && console.error('Failed to configure pages plugin', error);
     process.exit(-1);
-  })
+  });
 }
